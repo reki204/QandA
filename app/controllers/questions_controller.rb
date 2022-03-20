289505db3990c68771526c056d3cf0737566aa10
Onda Reki <77719187+reki204@users.noreply.github.com>
2022-03-21 08:33:24 +0900

    devise導入、userモデル、view作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -58,6 +58,6 @@ class QuestionsController < ApplicationController
 
   private
     def question_params
-      params.require(:question).permit(:title, :body, :image, :genre)
+      params.require(:question).permit(:title, :body, :image)
     end
 end
