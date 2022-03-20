0fa25ce9346d43dcac7b5d5139f3b3c7201dad6f
Onda Reki <77719187+reki204@users.noreply.github.com>
2022-03-20 22:34:48 +0900

    questionsコントローラー作成、view編集、model作成

---
+++ app/models/question.rb
@@ -0,0 +1,12 @@
+class Question < ApplicationRecord
+  belongs_to :user
+  has_many :answers, dependent: :destroy
+
+  mount_uploader :image, ImageUploader
+
+  with_options presence: true do
+    validates :title
+    validates :body
+    validates :genre
+  end
+end
