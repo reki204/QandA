819d3a9239b755642a2e6a93973018293401ad5f
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-08-06 20:14:07 +0900

    requests/answer編集

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
+  describe "GET #index" do
     it 'return http success' do
       get '/'
       expect(response).to have_http_status(:success)
